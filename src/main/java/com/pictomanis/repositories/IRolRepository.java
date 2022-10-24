1b38939a86b8d76da697e81f4d6d3d822bcb4afe
RenaPad <89104868+RenaPad@users.noreply.github.com>
2022-10-23 19:57:37 -0500

    Modificación del Repositories Rol

--- src/main/java/com/pictomanis/repositories/IRolRepository.java
+++ src/main/java/com/pictomanis/repositories/IRolRepository.java
@@ -1,12 +1,16 @@
 package com.pictomanis.repositories;
 
 import com.pictomanis.entities.Rol;
+import com.pictomanis.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface IRolRepository extends JpaRepository<Rol,Integer> {
-    List<Rol> findName(String nameRol);
+    @Query("from Rol u where u.nameRol like %:nameRol")
+    List<Rol> findName(@Param("nameRol") String nameRol);
 }
