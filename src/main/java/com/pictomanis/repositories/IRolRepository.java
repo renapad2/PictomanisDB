059d403dcb474c197acfcbbde52ddd4c2850b37b
RenaPad <89104868+RenaPad@users.noreply.github.com>
2022-10-23 21:43:06 -0500

    Modificación del Repositories Rol

--- src/main/java/com/pictomanis/repositories/IRolRepository.java
+++ src/main/java/com/pictomanis/repositories/IRolRepository.java
@@ -1,7 +1,6 @@
 package com.pictomanis.repositories;
 
 import com.pictomanis.entities.Rol;
-import com.pictomanis.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
